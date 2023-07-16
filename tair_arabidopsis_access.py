334c21fbaea3b01d350ad39d9541114ee3af3f56
Gaurav Sablok <sablokg@gmail.com>
2023-07-16 10:54:31 +0530

    changed the url to the recent url from the tair

--- tair_arabidopsis_access.py
+++ tair_arabidopsis_access.py
@@ -31,32 +31,32 @@ import wget
 #agi_uniprot_url = "https://www.arabidopsis.org/download_files/Proteins/Id_conversions/AGI2uniprot-JAN2023.txt"
 #uniprot_tair_url = "https://www.arabidopsis.org/download_files/Proteins/Id_conversions/TAIR2UniprotMapping-JAN2023.txt"
 if uniprot_agi:
-    uniprot_agi_url = "https://www.arabidopsis.org/download_files/Proteins/Id_conversions/Uniprot2AGI-JAN2023.txt"
+    uniprot_agi_url = "https://www.arabidopsis.org/download_files/Proteins/Id_conversions/Uniprot2AGI-Jul2023.txt"
     wget.download(uniprot_agi_url)
     uniprot_agi = {}
-    with open(os.path.abspath(os.path.join(os.getcwd(), "Uniprot2AGI-JAN2023.txt")), "r") as uni:
+    with open(os.path.abspath(os.path.join(os.getcwd(), "Uniprot2AGI-Jul2023.txt")), "r") as uni:
         for line in uni.readlines():
             uniprot_agi["uniprot", line.strip().split("\t")[0]] = \
                              {"AGI":line.strip().split("\t")[1:]}
-    os.remove(os.path.abspath(os.path.join(os.getcwd(), "Uniprot2AGI-JAN2023.txt"))
+    os.remove(os.path.abspath(os.path.join(os.getcwd(), "Uniprot2AGI-Jul2023.txt"))
 if agi_uniprot:
-    agi_uniprot_url = "https://www.arabidopsis.org/download_files/Proteins/Id_conversions/AGI2uniprot-JAN2023.txt"
+    agi_uniprot_url = "https://www.arabidopsis.org/download_files/Proteins/Id_conversions/AGI2uniprot-Jul2023.txt"
     wget.download(agi_uniprot_url)
     agi_uniprot = {}
-    with open(os.path.abspath(os.path.join(os.getcwd(), "AGI2uniprot-JAN2023.txt")), "r") as agi:
+    with open(os.path.abspath(os.path.join(os.getcwd(), "AGI2uniprot-Jul2023.txt")), "r") as agi:
         for line in agi.readlines():
             agi_uniprot["AGI",line.strip().split("\t")[0]] = \
                               {"uniprot_identifier": line.strip().split("\t")[1:]}
-    os.remove(os.path.join(os.getcwd(), "AGI2uniprot-JAN2023.txt"))                          
+    os.remove(os.path.join(os.getcwd(), "AGI2uniprot-Jul2023.txt"))                          
 if uniprot_tair:
-    uniprot_tair_url = "https://www.arabidopsis.org/download_files/Proteins/Id_conversions/TAIR2UniprotMapping-JAN2023.txt"
+    uniprot_tair_url = "https://www.arabidopsis.org/download_files/Proteins/Id_conversions/TAIR2UniprotMapping-Jul2023.txt"
     wget.download(uniprot_tair_url)
     uniprot_tair = {}
-    with open(os.path.abspath(os.path.join(os.getcwd(), "TAIR2UniprotMapping-JAN2023.txt")), "r") as tair:
+    with open(os.path.abspath(os.path.join(os.getcwd(), "TAIR2UniprotMapping-Jul2023.txt")), "r") as tair:
         for line in tair.readlines():
             tair_uniprot["uniprot", line.strip().split("\t")[0]] = \
                             {"locus_tair_ID":line.strip().split("\t")[1:]}
-    os.remove(os.path.join(os.getcwd(), "TAIR2UniprotMapping-JAN2023.txt"))                        
+    os.remove(os.path.join(os.getcwd(), "TAIR2UniprotMapping-Jul2023.txt"))                        
 if tair_protein_models:
     tair_protein_models = {}
     with open(os.path.abspath(os.path.join(os.getcwd(), "Araport11_pep_20220914_representative_gene_model")), "r") as protein:
@@ -73,9 +73,9 @@ if all_ua_au_ut_tpm:
                            availables in the present working directory")
             if i !="Uniprot2AGI-JAN2023.txt" and "AGI2uniprot-JAN2023.txt" and "TAIR2UniprotMapping-JAN2023.txt":
                 print(f"the annotation files from the TAIR are not present and fetching them for the analysis")
-                uniprot_agi_url = "https://www.arabidopsis.org/download_files/Proteins/Id_conversions/Uniprot2AGI-JAN2023.txt"
-                agi_uniprot_url = "https://www.arabidopsis.org/download_files/Proteins/Id_conversions/AGI2uniprot-JAN2023.txt"
-                uniprot_tair_url = "https://www.arabidopsis.org/download_files/Proteins/Id_conversions/TAIR2UniprotMapping-JAN2023.txt"
+                uniprot_agi_url = "https://www.arabidopsis.org/download_files/Proteins/Id_conversions/Uniprot2AGI-Jul2023.txt"
+                agi_uniprot_url = "https://www.arabidopsis.org/download_files/Proteins/Id_conversions/AGI2uniprot-Jul2023.txt"
+                uniprot_tair_url = "https://www.arabidopsis.org/download_files/Proteins/Id_conversions/TAIR2UniprotMapping-Jul2023.txt"
                 wget.download(uniprot_agi_url)
                 wget.download(agi_uniprot_url)
                 wget.download(uniprot_tair_url)
@@ -84,21 +84,21 @@ uniprot_agi = {}
 agi_uniprot = {}
 uniprot_tair = {}
 tair_protein_models = {}
-with open(os.path.abspath(os.path.join(os.getcwd(), "Uniprot2AGI-JAN2023.txt")), "r") as uni:
+with open(os.path.abspath(os.path.join(os.getcwd(), "Uniprot2AGI-Jul2023.txt")), "r") as uni:
         for line in uni.readlines():
             uniprot_agi["uniprot", line.strip().split("\t")[0]] = \
                              {"AGI":line.strip().split("\t")[1:]}
-os.remove(os.path.abspath(os.path.join(os.getcwd(), "Uniprot2AGI-JAN2023.txt"))                             
-with open(os.path.abspath(os.path.join(os.getcwd(), "AGI2uniprot-JAN2023.txt")), "r") as agi:
+os.remove(os.path.abspath(os.path.join(os.getcwd(), "Uniprot2AGI-Jul2023.txt"))                             
+with open(os.path.abspath(os.path.join(os.getcwd(), "AGI2uniprot-Jul2023.txt")), "r") as agi:
         for line in agi.readlines():
             agi_uniprot["AGI",line.strip().split("\t")[0]] = \
                                 {"uniprot_identifier": line.strip().split("\t")[1:]}
-os.remove(os.path.join(os.getcwd(), "AGI2uniprot-JAN2023.txt"))                                 
-with open(os.path.abspath(os.path.join(os.getcwd(), "TAIR2UniprotMapping-JAN2023.txt")), "r") as tair:
+os.remove(os.path.join(os.getcwd(), "AGI2uniprot-Jul2023.txt"))                                 
+with open(os.path.abspath(os.path.join(os.getcwd(), "TAIR2UniprotMapping-Jul2023.txt")), "r") as tair:
         for line in tair.readlines():
             tair_uniprot["uniprot", line.strip().split("\t")[0]] = \
                             {"locus_tair_ID":line.strip().split("\t")[1:]}
-os.remove(os.path.join(os.getcwd(), "TAIR2UniprotMapping-JAN2023.txt"))                             
+os.remove(os.path.join(os.getcwd(), "TAIR2UniprotMapping-Jul2023.txt"))                             
 with open(os.path.abspath(os.path.join(os.getcwd(), "Araport11_pep_20220914_representative_gene_model")), "r") as protein:
         for line in protein.readlines():
             if line.startswith(">"):
